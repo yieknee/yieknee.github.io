c0a47021dbeb867d7c1f56c11695c1e2316fd1b2
CheezItMan <chris@adadevelopersacademy.org>
2018-06-06 09:00:27 -0700

    change URL

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header>
           <h1>Ada&apos;s Inspiration Board</h1>
         </header>
-        <Board url="https://inspiration-bard.herokuapp.com/boards/"
+        <Board url="https://inspiration-board.herokuapp.com/boards/"
  boardId={1} />
       </div>
     );
