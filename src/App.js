465b736cf2185891f3801d157bdac59121e86c6d
CheezItMan <chris@adadevelopersacademy.org>
2020-04-07 20:00:50 -0700

    move App to functional component and ... empty out the test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Board from './components/Board';
 
-class App extends Component {
-  render() {
-    return (
-      <section>
-        <header className="header">
-          <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
-        </header>
-        <Board
-          url="https://inspiration-board.herokuapp.com/boards/"
-          boardName={`Ada-Lovelace`}
-          />
-      </section>
-    );
-  }
-}
+const App = () => {
+  return (
+    <section>
+      <header className="header">
+        <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
+      </header>
+      <Board
+        url="https://inspiration-board.herokuapp.com/boards/"
+        boardName={`Ada-Lovelace`}
+      />
+    </section>
+  );
+};
 
 export default App;
