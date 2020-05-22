44e4cfd417b2433058c84986468802a185171956
Yieni <yaynee8d@gmail.com>
2020-05-21 20:32:03 -0700

    Updates

---
+++ precache-manifest.ed7b4e65f00f8d566f68cffb8d49be02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcf1199de8465ae27c6098654c4bd95",
+    "url": "/yieknee/yieknee.github.io.git/index.html"
+  },
+  {
+    "revision": "0244e00c0b63929065ba",
+    "url": "/yieknee/yieknee.github.io.git/static/css/main.20192867.chunk.css"
+  },
+  {
+    "revision": "86e2267be3b53bdbad83",
+    "url": "/yieknee/yieknee.github.io.git/static/js/2.0b91450a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yieknee/yieknee.github.io.git/static/js/2.0b91450a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0244e00c0b63929065ba",
+    "url": "/yieknee/yieknee.github.io.git/static/js/main.ab54513b.chunk.js"
+  },
+  {
+    "revision": "7937820248cf4f78efca",
+    "url": "/yieknee/yieknee.github.io.git/static/js/runtime-main.4fc7fec2.js"
+  }
+]);
